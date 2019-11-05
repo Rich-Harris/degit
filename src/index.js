b2ed9788b9d6efd8ff6c2d67fdb9903abf3c131c
Yogev Boaron Ben-Har <yogiboaron@gmail.com>
2019-11-05 23:42:07 +0200

    Fix a typo

--- src/index.js
+++ src/index.js
@@ -300,7 +300,7 @@ function parse(src) {
 	);
 	if (!supported.has(site)) {
 		throw new DegitError(
-			`degit supports GitHub, GitLab, sourehut and BitBucket`,
+			`degit supports GitHub, GitLab, Sourcehut and BitBucket`,
 			{
 				code: 'UNSUPPORTED_HOST',
 			}
