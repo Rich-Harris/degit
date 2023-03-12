76fc68b214c828cb6eb399411dfc1182215ef013
zonaj2 <125164561+zonaj2@users.noreply.github.com>
2023-03-12 19:00:06 +0000

    change task background to red

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -30,6 +30,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           readOnly={true}
           name="title"
           placeholder="Input title"
+          style={{background: 'red'}}
         />
       </label>
 
