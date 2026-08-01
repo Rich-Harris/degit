b1b463e868eeb16f27715f5e26bb8a05bcb43f34
Haohan Yang <37651510+haohanyang@users.noreply.github.com>
2026-08-01 12:08:09 +0200

    refactor: create new variable `decodedSrc`

--- src/domain/repo.ts
+++ src/domain/repo.ts
@@ -161,9 +161,9 @@ function parseWebPath(
 }
 
 export function parse(src: string): Repo {
-	src = decodeURIComponent(src);
-	const [source, refValue = 'HEAD'] = src.split('#', 2);
-	const { remainder, site, transport, customDomain, isWebUrl } = resolveSource(source, src);
+	const decodedSrc = decodeURIComponent(src);
+	const [source, refValue = 'HEAD'] = decodedSrc.split('#', 2);
+	const { remainder, site, transport, customDomain, isWebUrl } = resolveSource(source, decodedSrc);
 
 	if (!supported.has(site)) {
 		throw new DegitError(`degit supports GitHub, GitLab, Sourcehut and BitBucket`, {
